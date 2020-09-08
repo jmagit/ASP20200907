2e9dc9b0eecb306477d59672e49d9174e8e0966d
Javier <jmartin@grupoloyal.com>
2020-09-08 09:04:21 +0200

    Imagenes

---
+++ Curso/Models/Core.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Domain.Entities.Core {
+    public abstract class Entity {
+
+    }
+}
+namespace Domain.Services.Core {
+    public interface IDomainService<TEntity, Key>
+        where TEntity : class {
+        List<TEntity> GetAll();
+        TEntity GetOne(Key id);
+        void Add(TEntity item);
+        void Modify(TEntity item);
+        void Remove(TEntity item);
+    }
+}
+namespace Domain.Core {
+    public interface IRepository<TEntity, Key>
+        where TEntity : class {
+        List<TEntity> GetAll();
+        TEntity GetOne(Key id);
+        void Add(TEntity item);
+        void Modify(TEntity item);
+        void Remove(TEntity item);
+    }
+}
+namespace Infrastructure.Data.Core.Repositories {
+}
